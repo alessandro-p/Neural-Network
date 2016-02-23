0ae69125a19eb72a38ce076096cfe060324d035d
alessandro-p <alessandro.petraro@gmail.com>
2016-02-23 15:06:37 +0100

    Initial Commit

---
+++ Neural-network-project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Neural-network-project
+//
+//  Created by Alessandro Petraro on 23/02/16.
+//  Copyright © 2016 Alessandro Petraro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
